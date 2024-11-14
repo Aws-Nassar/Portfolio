f461f9f89d9757e14fc1778286b2b4234de329a8
Aws Nassar <166246247+Aws-Nassar@users.noreply.github.com>
2024-11-14 13:35:21 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const map = L.map('map').setView([32.1797, 34.9664], 14);  // الإحداثيات المعدلة
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
